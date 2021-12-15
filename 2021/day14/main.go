84356e1fbfcbee3260e86c08a94840e2ed42bc30
Torben Schinke <torben.schinke@neotos.de>
2021-12-15 22:09:50 +0100

    day14p2: missed input double count bug

--- 2021/day14/main.go
+++ 2021/day14/main.go
@@ -18,41 +18,15 @@ func main() {
 		log.Fatalf("cannot parse file: %v\n", err)
 	}
 
-	fmt.Println(part1(seed, 10, rules))
-	fmt.Println("--")
-	fmt.Println(part2(seed, 40, rules)) //test is correct but actual sample not
-
+	fmt.Println(apply(seed, 10, rules))
+	fmt.Println(apply(seed, 40, rules))
 }
 
-func part1(seed string, iterations int, rules []Rule) int {
-	str := SliceBuf(seed)
-
-	for step := 0; step < iterations; step++ {
-		fmt.Println(step, len(str))
-	nextPair:
-		for i := 0; i < len(str)-1; i += 2 {
-			for _, rule := range rules {
-				if str[i] == rule.pattern[0] && str[i+1] == rule.pattern[1] {
-					//str.join(str[:i], rule.char, str[i:])
-					str.insert(i+1, rule.char)
-					//i++
-					//	fmt.Println("inserting", string(rule.char))
-					continue nextPair
-				}
-			}
-
-		}
-		//fmt.Println(string(str))
-	}
-
-	min, max := str.count()
-	return max - min
-}
-
-func part2(seed string, iterations int, rules []Rule) int {
+func apply(seed string, iterations int, rules []Rule) int {
 	counts := map[[2]byte]int{}
 	for i := 0; i < len(seed)-1; i++ {
-		counts[[2]byte{seed[i], seed[i+1]}] = 1
+		k := [2]byte{seed[i], seed[i+1]}
+		counts[k] = counts[k] + 1
 	}
 
 	for step := 0; step < iterations; step++ {
@@ -63,16 +37,11 @@ func part2(seed string, iterations int, rules []Rule) int {
 					left, right := [2]byte{rule.pattern[0], rule.char}, [2]byte{rule.char, rule.pattern[1]}
 					cleft, cright := tmp[left], tmp[right]
 					tmp[left], tmp[right] = c+cleft, c+cright
-					fmt.Printf("%s => %s(%d) - %s(%d)\n", string(rule.pattern[:]), string(left[:]), c+cleft, string(right[:]), c+cright)
 				}
 			}
 		}
 
 		counts = tmp
-	}
-
-	for bytes, i := range counts {
-		fmt.Println(string(bytes[:]), i)
 	}
 
 	flat := deflate(counts)
@@ -117,39 +86,6 @@ func parse(fname string) (seed string, rules []Rule, err error) {
 type Rule struct {
 	pattern [2]byte
 	char    byte
-}
-
-func (r Rule) String() string {
-	return string(r.pattern[:]) + " -> " + string(r.char)
-}
-
-type SliceBuf []byte
-
-func (s *SliceBuf) join(a []byte, c byte, b []byte) {
-	tmp := (*s)[:0]
-	tmp = append(tmp, a...)
-	tmp = append(tmp, c)
-	tmp = append(tmp, b...)
-	*s = tmp
-}
-
-func (s *SliceBuf) insert(pos int, v byte) {
-	tmp := *s
-	tmp = append(tmp, 0)
-	copy(tmp[pos+1:], tmp[pos:])
-	tmp[pos] = v
-	*s = tmp
-}
-
-func (s *SliceBuf) count() (min, max int) {
-	count := map[byte]int{}
-	for _, b := range *s {
-		count[b] = count[b] + 1
-	}
-
-	min, max = minMax(count)
-
-	return
 }
 
 func minMax(count map[byte]int) (min, max int) {
